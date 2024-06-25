241ef84173fdd935c246928483a8aaff4da7a969
Rageblade-21 <80586780+Rageblade-21@users.noreply.github.com>
2024-06-24 23:02:03 -0400

    V1.1
    Updated Script to check players hands for weapons every 1 second

---
+++ movesItemsToQBInventory/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Rageblade'
+description 'Moves All weapons that a player is holding to thier inventory'
+version '1.1.0'
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
